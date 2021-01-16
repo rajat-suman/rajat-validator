2ba0d9e77fcdb9c5c71e78fe5ed146010cb1e769
Rajat Suman <rsuman1997@gmail.com>
2021-01-16 14:02:22 +0530

    example

--- app/src/main/java/com/rajat/easyvalidations/MainActivity.kt
+++ app/src/main/java/com/rajat/easyvalidations/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.rajat.easyvalidations
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.rajat.validator.ErrorType
 import com.rajat.validator.Validator
@@ -9,13 +10,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setValidations()
+    }
 
-        val validator = Validator.create(activity = null).isEmpty("", "avdd")
-        validator?.isValid(ErrorType.TOAST)
-        validator?.isValid(ErrorType.SNACKBAR)
-        validator?.isValid(ErrorType.DIALOG)
-        validator?.isValid {
+    private fun setValidations() = try {
+        val etDemo = findViewById<EditText>(R.id.etDemo)
+        val etDemo2 = findViewById<EditText>(R.id.etDemo)
+        val validator = Validator.create(activity = this).apply {
+            isEmpty(etDemo.text.toString(), "Empty")
+            isEmailValid(etDemo.text.toString(), "Invalid Email")
+            isPhoneValid(etDemo.text.toString(), "Invalid Phone number")
+            isValidPassword(
+                etDemo.text.toString(),
+                "Password must be of atleast 8 characters and must contains One Upper case, One Special character and One Numeric"
+            )
+            areEqual(etDemo.text.toString(), etDemo2.text.toString(), "Not Equal")
+        }
+
+        // For Errors in Toasts
+        if (validator.isValid(ErrorType.TOAST)) {
+            //Do Stuff
+        }
+
+        //For Errors in SnackBars
+        if (validator.isValid(ErrorType.SNACKBAR)) {
+            //Do Stuff
+        }
+
+        //For Errors in Dialogs
+        if (validator.isValid(ErrorType.DIALOG)) {
+            //Do Stuff
+        }
+
+        //for Getting error messages List
+        validator.isValid {
 
         }
+    } catch (e: Exception) {
+        e.printStackTrace()
     }
 }
